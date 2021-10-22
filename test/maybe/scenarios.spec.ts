5ba12e8d2c495253f9c6e7162682b805705b493a
Sean Wright <sean@seangwright.me>
2021-10-22 18:41:23 -0400

    test(Maybe): add scenario example

---
+++ test/maybe/scenarios.spec.ts
@@ -0,0 +1,46 @@
+import { Maybe } from '@/src/maybe';
+
+describe('Maybe', () => {
+  describe('scenarios', () => {
+    test('nothing is done when there is no value', async () => {
+      let functionCalls = 0;
+
+      await Maybe.tryFirst(getUsers(), (u) => u.hasConfirmedAccount)
+        .tapAsync(sendPromotionalEmail)
+        .bind(({ referralAccount }) => referralAccount)
+        .tapAsync(sendPromotionalEmail)
+        .toPromise();
+
+      function sendPromotionalEmail(user: User): Promise<void> {
+        functionCalls++;
+        return Promise.resolve();
+      }
+
+      expect(functionCalls).toBe(0);
+    });
+  });
+});
+
+type User = {
+  email: string;
+  firstName: string;
+  lastName: string;
+  id: string;
+  roles: string[];
+  hasConfirmedAccount: boolean;
+  referralAccount: Maybe<User>;
+};
+
+function getUsers(): User[] {
+  return [
+    {
+      email: 'test@test.com',
+      firstName: 'Test',
+      lastName: 'User',
+      hasConfirmedAccount: false,
+      referralAccount: Maybe.none(),
+      id: '1234',
+      roles: ['user'],
+    },
+  ];
+}
